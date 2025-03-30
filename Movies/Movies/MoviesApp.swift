ba4741b889b3665e910d327b3ed4052b36e8a86c
Artur Harutyunyan <arturhar@outlook.com>
2025-03-30 13:49:07 +0400

    Start

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Artur Harutyunyan on 30.03.25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
